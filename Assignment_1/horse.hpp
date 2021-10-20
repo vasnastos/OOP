3cec4cb7dcaa7873c8da157f3c55f0129b486672
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2021-10-20 14:08:20 +0300

    Add files via upload

---
+++ Assignment_1/horse.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class horse
+{
+    private:
+        int id;
+        std::string name;
+        int power;
+        int startup_speed;
+        int stamina;
+        int position;
+    public:
+        horse(int h_id,std::string h_name,int h_power,int h_startup_speed,int h_stamina);
+        ~horse() {}
+        bool move_forward(int step,int random_factor);
+        bool is_finished(int &number_of_rounds);
+
+        // Set and Get
+        void set_position(int h_position);
+        int get_position()const;
+        std::string get_name()const;
+        int get_id()const;
+};
