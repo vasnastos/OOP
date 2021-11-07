5d36c1bae9ff0264a3ae191d8ea2053906ae9d46
Dimitrios Gkiokezas <int01886@uoi.gr>
2021-11-07 13:30:28 +0200

    Update Lesson_2

--- Lesson_2/Vectors/vector8.cpp
+++ Lesson_2/Vectors/vector8.cpp
@@ -1,47 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <chrono>
-#include <random>
+#include <iterator>
+#include <algorithm> //sort()
 
 using namespace std;
 
 int main()
 {
-    vector<int> vec;
-    vector<int> vec2;
-    for (int i = 0; i < 5; i++)
+    vector <pair<string, int>> vec;
+    vec.push_back(make_pair("Petros", 23));
+    vec.push_back(make_pair("Giwrgos",25));
+    vec.push_back(make_pair("Maria", 19));
+    vec.push_back(make_pair("Kwstas", 48));
+    vec.push_back(make_pair("Nikos", 32));
+    vec.push_back(make_pair("Katerina", 56));
+
+    for(auto it=0; it!=vec.size(); it++)
     {
-        vec.push_back(i);
-        vec2.push_back(i);
+        cout<<"vec["<<it<<"] = "<<vec[it].first <<" : " <<vec[it].second<<endl;
+        //cout<<vec[it].first <<":" <<vec[it].second<<endl;
     }
 
-    int j = 0;
-    cout << "Before shuffle" << endl;
-    for (int j = 0; j < 5; j++)
-    {
-        cout << "vec[" << j << "] = " << vec.at(j) << "  vec2[" << j << "] = " << vec2.at(j) << endl;
-    }
+    
 
-    int i = 0;
 
-/*
-    random_shuffle(vec.begin(), vec.end());
-    cout << "After random_shuffle()" << endl;
-    for (auto it = vec.begin(); it != vec.end(); ++it)
-    {
-        cout << "vec2[" << i << "] = " << *it << endl;
-        i++;
-    }
-*/
-
-    i = 0;
-    auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-    shuffle(vec2.begin(), vec2.end(), std::default_random_engine(seed));
-    cout << "After shuffle()" << endl;
-    for (auto it = vec2.begin(); it != vec2.end(); ++it)
-    {
-        cout << "vec2[" << i << "] = " << *it << endl;
-        i++;
-    }
 }
